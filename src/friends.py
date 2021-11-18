1e8be0edd9cdeb22f00eb88e4a229b0b7992df0a
Jamie <jamienolan93@hotmail.co.uk>
2021-11-18 15:21:10 +0000

    finally did number 4

--- src/friends.py
+++ src/friends.py
@@ -4,4 +4,12 @@ def get_name(person):
 def get_favourite_tv_show(tv_show):
     return tv_show["favourites"]["tv_show"]
 
+def likes_to_eat(person, food):
+    if food in person["favourites"]["snacks"]:
+        return True
+    return False
+
+def add_friend(person, name):
+    person["friends"].append(name)
+    
 
